094b052a6080da440b41e1c73be2a8540a4e9298
Gabriel Soares <gabrielso4res@gmail.com>
2021-04-26 15:22:03 -0300

    Desafio upado. Testando se a conta ja existe, criando usuario, listando todos e criando todos

---
+++ src/index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const cors = require('cors');
+
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+
+function checksExistsUserAccount(request, response, next) {
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user){
+    return response.status(400).json({ error: "User not found!" })
+  }
+
+  request.user = user;
+
+  return next();
+}
+
+app.post('/users', (request, response) => {
+  const { name, username } = request.body;
+
+  const userAlreadyExists = users.some((user) => user.username === username);
+
+  if(userAlreadyExists){
+    return response.status(400).json({ error: "User already exists!"})
+  }
+  users.push({
+    name,
+    username,
+    id: uuidv4(),
+    todos: []
+  })
+
+  return response.status(201).send();
+});
+
+app.get('/todos', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+
+  return response.json(user.todos);
+});
+
+app.post('/todos', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { title, deadline } = request.body;
+
+  const todoOperation = {
+    id: uuidv4,
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  user.todos.push(todoOperation);
+
+  return response.status(201).send();
+});
+
+app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+  
+});
+
+app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+app.listen(3333);
+
+module.exports = app;
