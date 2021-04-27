80021192aa94dd82565acdc6f73c1713e71e51ce
Gabriel Soares <gabrielso4res@gmail.com>
2021-04-27 08:53:14 -0300

    erro no put todo

--- src/index.js
+++ src/index.js
@@ -45,6 +45,7 @@ app.post('/users', (request, response) => {
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   const { user } = request;
 
+
   return response.json(user.todos);
 });
 
@@ -66,7 +67,20 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { title, deadline } = request.body;
+  const { id } = request.params;
   
+  user.todos.find(todo => todo.id === id);
+
+  if(!todo){
+    return response.status(400).json({ error:"Todo not found!" })
+  }
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.json(todo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
